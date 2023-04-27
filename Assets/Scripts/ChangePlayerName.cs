28a5a1212e4f3ac78240f60af7cbb1750031073a
F. N. Konstantin Burger <konsti.burger@gmail.com>
2023-04-27 22:55:57 +0200

    Created GameManager script

---
+++ Assets/Scripts/ChangePlayerName.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePlayerName : MonoBehaviour
+{
+    //[SerializeField] TextArea textArea;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    if (!PlayerPrefs.HasKey("player1_name"))
+    //    {
+    //        PlayerPrefs.SetFloat("player1_name", textArea.getString());
+    //    }
+    //    else
+    //    {
+    //        Load();
+    //    }
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    //public void ChangeName()
+    //{
+    //    Save();
+    //}
+
+    //private void Load()
+    //{
+    //    textArea.getString() = PlayerPrefs.GetFloat("player1_name");
+    //}
+
+    //private void Save()
+    //{
+    //    PlayerPrefs.SetFloat("player1_name", textArea.getString());
+    //}
+}
