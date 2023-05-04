b367703f3dc9b33b9b34cfd3a468ecc4b73ff14a
Aqare <aqarecover@gmx.at>
2023-05-05 00:23:50 +0200

    a

--- Assets/Scripts/MainPlayerScript.cs
+++ Assets/Scripts/MainPlayerScript.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class MainPlayerScript : MonoBehaviour
 {
 
-    public AudioSource sfx_source;
+    public AudioSource sfx_source;              //Item get sfx
+    [SerializeField] GameObject item_get_particles;
 
-    
+
+    //This variable holds the current item.
+    //If it's -1, the player is not holding an item. When using items, you need to set it back to -1.
+    //Set when getting an item in OnTriggerEnter event.
+    public int current_item = -1;
+
+
 
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
@@ -18,12 +25,23 @@ public class MainPlayerScript : MonoBehaviour
     {
         print("entered");
 
+
+        //ITEM COLLISION
         if (other.gameObject.layer == 12)
         {
+            //PLAY SFX
             sfx_source = GetComponent<AudioSource>();
             sfx_source.Play();
-            print("Got item");
+
+            //GIVE PLAYER AN ITEM
+            current_item = Random.Range(0, 3);
+            print("---GOT ITEM: " + current_item);
+
+
+            //CREATE PARTICLES AND DESTROY ITEM BOX
+            GameObject particles = Instantiate(item_get_particles, transform.position, transform.rotation);
             Destroy(other.gameObject);
+            Destroy(particles, 0.7f);
         }
     }
 
@@ -48,6 +66,6 @@ public class MainPlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+   
     }
 }
