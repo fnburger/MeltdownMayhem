97c17421483aaa0ba95a0d5fc12b97ac75d284dc
F. N. Konstantin Burger <konsti.burger@gmail.com>
2023-05-20 23:43:01 +0200

    fixed "called Move() on inactive controller" message

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -25,15 +25,4 @@ public class InputHandler : MonoBehaviour, AxisState.IInputAxisProvider
     }
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
